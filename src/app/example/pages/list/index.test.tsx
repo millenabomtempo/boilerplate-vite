99ba2e192565fd5596e9be3e68f3df6482ca216a
Millena B Bomtempo <millena.bomtempo@gmail.com>
2025-05-01 13:47:43 -0300

    fix: fix example test

--- src/app/example/pages/list/index.test.tsx
+++ src/app/example/pages/list/index.test.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { lazy } from 'react'
 import { describe, expect, it, vi } from 'vitest'
 
-const List = lazy(() => import('./index'))
+import List from './index'
 
-const renderComponent = (queryClient: QueryClient) => {
+const renderComponent = () => {
+  const queryClient = new QueryClient()
+
   return render(
     <QueryClientProvider client={queryClient}>
       <List />
@@ -16,56 +17,38 @@ const renderComponent = (queryClient: QueryClient) => {
 
 describe('<List  />', () => {
   it('should render page', () => {
-    const queryClient = new QueryClient()
-
-    const component = renderComponent(queryClient)
+    const component = renderComponent()
 
     expect(component).toBeTruthy()
   })
 
   it('should show error message', async () => {
-    const user = userEvent.setup()
-    const queryClient = new QueryClient()
+    renderComponent()
 
-    renderComponent(queryClient)
+    expect(await screen.findByTestId('text--title')).toBeInTheDocument()
 
-    await waitFor(() => {
-      expect(screen.getByTestId('text--title')).toBeInTheDocument()
-    })
+    userEvent.click(await screen.findByTestId('button--submit'))
 
-    await user.click(screen.getByTestId('button--submit'))
-
-    await waitFor(() => {
-      expect(screen.getByText('Name is required')).toBeInTheDocument()
-      expect(screen.getByText('Age is required')).toBeInTheDocument()
-    })
+    expect(await screen.findByText('Name is required')).toBeInTheDocument()
+    expect(await screen.findByText('Age is required')).toBeInTheDocument()
   })
 
   it('should call onSubmit when form is submitted', async () => {
     const user = userEvent.setup()
     const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
 
-    const queryClient = new QueryClient()
+    renderComponent()
 
-    renderComponent(queryClient)
-
-    await waitFor(() => {
-      expect(screen.getByTestId('text--title')).toBeInTheDocument()
-    })
+    expect(await screen.findByTestId('text--title')).toBeInTheDocument()
 
     await user.type(screen.getByTestId('input--name'), 'John Doe')
     await user.type(screen.getByTestId('input--age'), '20')
 
     await user.click(screen.getByTestId('button--submit'))
 
-    await waitFor(() => {
-      expect(logSpy).toHaveBeenCalledWith({
-        data: expect.objectContaining({
-          name: 'John Doe',
-          age: '20',
-        }),
-      })
-    })
+    const expected = { data: { name: 'John Doe', age: '20' } }
+
+    expect(logSpy).toHaveBeenCalledWith(expected)
 
     logSpy.mockRestore()
   })
